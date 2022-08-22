83549e1336539ba7cc8652cb8cb3a79559bc3ee2
timmywheels <tim@timwheeler.com>
2022-08-21 20:56:04 -0400

    1.0.217

--- README.md
+++ README.md
@@ -2,14 +2,17 @@
 A monkey-patch library for the native JavaScript `fetch` API, inspired by [`fetch-intercept`](https://www.npmjs.com/package/fetch-intercept).
 
 ```typescript
-const fetchConfig = new MonkeyFetch();
-
 fetchConfig.configure({
   request: (resource, options) => {
     // add custom request handler
+    
     // example: add Authorization headers to all requests
     const newOptions = { ...options };
-    const { accessToken } = customAuthHook();
+    
+    // fetch accessToken from local store
+    const { accessToken } = localAuthStore();
+    
+    // add auth token to all fetch requests
     newOptions.headers = {
       ...newOptions.headers,
       Authorization: `Bearer ${accessToken}`,
@@ -22,14 +25,46 @@ fetchConfig.configure({
   },
   response: (response: Response) => {
     // add custom response handling
+    // example: check if response is a 401, if so
+    // reject and invoke response error handler
     if (response.status === 401) {
-      throw new Error('Access token expired');
+      return Promise.reject(response);
     }
     return response;
   },
-  responseError: (error: Error) => {
-    // add custom response error handling
-    return Promise.reject(error);
+  responseError: async (response: IMonkeyFetchResponse) => {
+    const { request } = response;
+    // example: if response returned 401
+    // fetch a new accessToken
+    if (response.status === 401) {
+
+      // your custom methods for handling accessToken
+      const { accessToken, refreshToken, setAccessToken } = localAuthStore();
+      
+      const res = await fetch('http://localhost:5000/api/v1/auth/refresh', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ refreshToken })
+      });
+
+      // parse the response from your auth endpoint
+      const { accessToken: newAccessToken } = await res.json();
+
+      
+      setAccessToken(accessToken);
+
+      // retry the original request
+      const originalRequest = request.clone();
+      return await fetch(originalRequest, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        }
+      });
+    }
+
+    return Promise.reject(response);
   },
 });
 ```
--- src/index.ts
+++ src/index.ts
@@ -4,30 +4,30 @@ interface IMonkeyFetchRequestConfiguration {
 }
 
 interface IMonkeyFetchResponseConfiguration {
-  response?: (response: Response) => Response;
-  responseError?: (error: Error) => Promise<any>;
+  response?: (response: Response) => Response | Promise<IMonkeyFetchResponse>;
+  responseError?: (response: IMonkeyFetchResponse) => Promise<any>;
 }
 
 interface IMonkeyFetchConfiguration extends IMonkeyFetchRequestConfiguration, IMonkeyFetchResponseConfiguration {
 }
 
-interface IMonkeyPatchResponse extends Response {
+interface IMonkeyFetchResponse extends Response {
   request: Request;
 }
 
 export class MonkeyFetch {
   interceptors: IMonkeyFetchConfiguration = {
-    request: (resource: RequestInfo | URL, options: RequestInit) => Promise.resolve([resource, options]),
+    request: (resource: (RequestInfo | URL), options: RequestInit) => Promise.resolve([resource, options]),
     requestError: (error: Error) => Promise.reject(error),
     response: (response: Response) => response,
-    responseError: (error: Error) => Promise.reject(error),
+    responseError: (response: IMonkeyFetchResponse) => Promise.reject(response),
   };
 
   constructor() {
     this.init();
   }
 
-  protected init() {
+  protected init(): void {
     // check if current javascript context is browser or node runtime
     const isBrowserExecutionContext = typeof window !== 'undefined';
     if (isBrowserExecutionContext) {
@@ -37,7 +37,7 @@ export class MonkeyFetch {
     }
   }
 
-  protected applyInterceptors(fetch, ...args) {
+  protected applyInterceptors(fetch, ...args): Promise<any> {
     const {
       request,
       requestError,
@@ -48,15 +48,14 @@ export class MonkeyFetch {
     // init promise with original args proceed to monkey-patch
     let monkeyPatchedPromise: Promise<any> = Promise.resolve(args);
 
-    // register request interceptors
     monkeyPatchedPromise = monkeyPatchedPromise.then(
-      (args: [RequestInfo | URL, RequestInit]) => request(...args),
+      (args: [(RequestInfo | URL), RequestInit]) => request(...args),
       requestError,
     );
 
-    monkeyPatchedPromise = monkeyPatchedPromise.then((args: [RequestInfo | URL, RequestInit]) => {
+    monkeyPatchedPromise = monkeyPatchedPromise.then((args: [(RequestInfo | URL), RequestInit]) => {
       const request = new Request(...args);
-      return fetch(request).then((response: IMonkeyPatchResponse) => {
+      return fetch(request).then((response: IMonkeyFetchResponse): IMonkeyFetchResponse => {
         response.request = request;
         return response;
       }).catch((error) => {
@@ -65,14 +64,13 @@ export class MonkeyFetch {
       });
     });
 
-    // register response interceptors
-    monkeyPatchedPromise = monkeyPatchedPromise.then(response, responseError);
+    monkeyPatchedPromise = monkeyPatchedPromise.then(response).catch(responseError);
 
     return monkeyPatchedPromise;
   }
 
   // public api to configure fetch
-  configure(configuration: IMonkeyFetchConfiguration) {
+  configure(configuration: IMonkeyFetchConfiguration): void {
     for (let interceptorKey in configuration) {
       this.interceptors[interceptorKey] = configuration[interceptorKey];
     }
@@ -83,34 +81,3 @@ export class MonkeyFetch {
       this.applyInterceptors(fetch, ...args))(globalThis.fetch);
   }
 }
-
-const fetchConfig = new MonkeyFetch();
-
-fetchConfig.configure({
-  request: (resource, options) => {
-    // add custom request handler
-    // example: add Authorization headers to all requests
-    const newOptions = { ...options };
-    // const { accessToken } = customAuthHook();
-    newOptions.headers = {
-      ...newOptions.headers,
-      Authorization: `Bearer HELLO_WORLD`,
-    };
-    return Promise.resolve([resource, newOptions]);
-  },
-  requestError: (error: Error) => {
-    // add custom request error handler
-    return Promise.reject(error);
-  },
-  response: (response: Response) => {
-    // add custom response handling
-    if (response.status === 401) {
-      throw new Error('Access token expired');
-    }
-    return response;
-  },
-  responseError: (error: Error) => {
-    // add custom response error handling
-    return Promise.reject(error);
-  },
-});
